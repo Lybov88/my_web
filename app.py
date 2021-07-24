fe17ddacf5aa43e0bfe16738b6eecf2a0be6c717
Lybov88 <eL_Pi@mail.ru>
2021-07-24 12:54:31 +0900

    modified content

--- app.py
+++ app.py
@@ -7,43 +7,49 @@ app = Flask(__name__)
 # проверка переменной __name__
 # print(__name__)
 
+
 def hashing(password, num_char):
-	# кодирование
-	byte_str = password.encode()
-	# хеширование
-	hash_str = sha256(byte_str)
-	# преобразование в hex-строка
-	if num_char == '-':
-		hex_str = hash_str.hexdigest()
-	else:
-		hex_str = hash_str.hexdigest()[:int(num_char)]
-	# возврат строки
-	return hex_str
+    # кодирование
+    byte_str = password.encode()
+    # хеширование
+    hash_str = sha256(byte_str)
+    # преобразование в hex-строка
+    if num_char == '-':
+        hex_str = hash_str.hexdigest()
+    else:
+        hex_str = hash_str.hexdigest()[:int(num_char)]
+    # возврат строки
+    return hex_str
 
 # основная логика нашего сервера
+
+
 @app.route("/")
 def index_page():
-	# return "Hello, browser! I am Flask server!"
-	# возврат страницы
-	return render_template("index.html")
+    # return "Hello, browser! I am Flask server!"
+    # возврат страницы
+    return render_template("index.html")
+
 
 @app.route("/product", methods=["GET", "POST"])
 def product_page():
-	# возврат страницы
-	msg = ""
-	if request.method == "POST":
-		pwd = request.form.get("pwd")
-		salt = request.form.get("salt")
-		num_char = request.form.get("num")
+    # возврат страницы
+    msg = ""
+    if request.method == "POST":
+        pwd = request.form.get("pwd")
+        salt = request.form.get("salt")
+        num_char = request.form.get("num")
 
-		msg = hashing(pwd + salt, num_char)
+        msg = hashing(pwd + salt, num_char)
 
-	return render_template("product.html", message=msg)
+    return render_template("product.html", message=msg)
+
 
 @app.route("/contact")
 def contact_page():
-	return "<h1>Contact</h1>"
+    return render_template("contact.html")
+
 
 # точка входа
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
