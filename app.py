61902a3fd87c1f23fcc674a18079ba53483d4ceb
Lybov88 <eL_Pi@mail.ru>
2021-07-24 19:38:49 +0900

    создали БД

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
+from sqlite3.dbapi2 import Cursor
 from flask import Flask, render_template, request
 from hashlib import sha256
+import sqlite3 as sql
+
 
 # создание экземпляра сервера
 app = Flask(__name__)
 
 # проверка переменной __name__
 # print(__name__)
+
+# Создание БД
+with sql.connect("logs.db") as db:
+    cursor = db.cursor()
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS log_table (
+			form TEXT,
+			address TEXT,
+			user TEXT
+			result TEXT
+		)"""
+    )
+    db.commit()
 
 
 def hashing(password, num_char):
@@ -20,6 +36,17 @@ def hashing(password, num_char):
         hex_str = hash_str.hexdigest()[:int(num_char)]
     # возврат строки
     return hex_str
+
+
+def loging(req, res):
+    with sql.connect("logs.db") as db:
+        cursor = db.cursor()
+        cursor.execute(
+            "INSERT INTO log_table VALUES (?, ?, ?, ?)",
+            (str(req.form), str(req.remote_address), str(req, user_agent), res)
+        )
+        db.commit()
+
 
 # основная логика нашего сервера
 
@@ -42,12 +69,20 @@ def product_page():
 
         msg = hashing(pwd + salt, num_char)
 
+		loging(request, msg)
+
     return render_template("product.html", message=msg)
 
 
 @app.route("/contact")
 def contact_page():
-    return render_template("contact.html")
+	with sql.connect("logs.db") as db:
+		cursor = db.cursor()
+		cursor.execute(
+			"SELECT * FROM log_table"
+		)
+		data = cursor.fetchall()
+    return render_template("contact.html", data=data)
 
 
 # точка входа
---
+++ sqlite_example.py
@@ -0,0 +1,54 @@
+# *** Система управления базами данных (СУБД) SQLite3 ***
+
+
+import sqlite3 as sql
+from sqlite3.dbapi2 import Cursor
+
+# создание базы дынных с таблицей test
+# with sql.connect("test.db") as db:
+#     # объект курсора
+#     cursor = db.cursor()
+#     # передача команд на языке SQL
+#     cursor.execute(
+#         """CREATE TABLE IF NOT EXISTS test
+#         (login TEXT, id INTEGER)"""
+#     )
+#     # закрепление изменений
+#     db.commit()
+
+
+# запись в БД
+# with sql.connect("test.db") as db:
+#     # объект курсора ("панель управления")
+#     cursor = db.cursor()
+#     # передача команд на языке SQL
+#     cursor.execute(
+#         "INSERT INTO test VALUES (?, ?)",
+#         ("Helloo", 777)
+#     )
+# закрепление изменений
+# db.commit()
+
+
+#  чтение из БД
+with sql.connect("test.db") as db:
+    # объект курсора ("панель управления")
+    cursor = db.cursor()
+    # передача команд на языке SQL
+    cursor.execute(
+        "SELECT * FROM test"
+    )
+#     # чтение записи по одной
+#     result = cursor.fetchone()
+#     print(result)
+#     # чтение записи по одной
+#     result = cursor.fetchone()
+#     print(result)
+#     # чтение записи по одной
+#     result = cursor.fetchone()
+#     print(result)
+
+
+# чтение всех записей из таблицы
+result = cursor.fetchall()
+print(result)
