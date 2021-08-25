53688349c7928802cf962ed9cdc9a34563f5cf11
Lybov88 <eL_Pi@mail.ru>
2021-08-25 22:42:46 +0900

    добавили try-except

--- app.py
+++ app.py
@@ -9,72 +9,89 @@ app = Flask(__name__)
 # print(__name__)
 
 # создание БД
-with sql.connect("logs.db") as db:
-	cursor = db.cursor()
-	cursor.execute(
-		"""CREATE TABLE IF NOT EXISTS log_table (
-			form TEXT,
-			address TEXT,
-			user TEXT,
-			result TEXT
-		)"""
-	)
-	db.commit()
+try:
+    with sql.connect("logs.db") as db:
+        cursor = db.cursor()
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS log_table (
+				form TEXT,
+				address TEXT,
+				user TEXT,
+				result TEXT
+			)"""
+        )
+        db.commit()
+except sql.Error as err:
+    print(f"Ошибка БД: {err}")
+except Exception as err:
+    print(f"Что-то пошло не так: {err}")
+
 
 def hashing(password, num_char):
-	# кодирование
-	byte_str = password.encode()
-	# хеширование
-	hash_str = sha256(byte_str)
-	# преобразование в hex-строка
-	if num_char == '-':
-		hex_str = hash_str.hexdigest()
-	else:
-		hex_str = hash_str.hexdigest()[:int(num_char)]
-	# возврат строки
-	return hex_str
+    # кодирование
+    byte_str = password.encode()
+    # хеширование
+    hash_str = sha256(byte_str)
+    # преобразование в hex-строка
+    if num_char == '-':
+        hex_str = hash_str.hexdigest()
+    else:
+        hex_str = hash_str.hexdigest()[:int(num_char)]
+    # возврат строки
+    return hex_str
+
 
 def loging(req, res):
-	with sql.connect("logs.db") as db:
-		cursor = db.cursor()
-		cursor.execute(
-			"INSERT INTO log_table VALUES (?,?,?,?)",
-			(str(req.form), str(req.remote_addr), str(req.user_agent), res)
-		)
-		db.commit()
+    try:
+        with sql.connect("logs.db") as db:
+            cursor = db.cursor()
+            cursor.execute(
+                "INSERT INTO log_table VALUES (?,?,?,?)",
+                (str(req.form), str(req.remote_addr), str(req.user_agent), res)
+            )
+            db.commit()
+    except sql.Error as err:
+        print(f"Ошибка БД: {err}")
+    except Exception as err:
+        print(f"Что-то пошло не так: {err}")
 
 # основная логика нашего сервера
+
+
 @app.route("/")
 def index_page():
-	# return "Hello, browser! I am Flask server!"
-	# возврат страницы
-	return render_template("index.html")
+    # return "Hello, browser! I am Flask server!"
+    # возврат страницы
+    return render_template("index.html")
+
 
 @app.route("/product", methods=["GET", "POST"])
 def product_page():
-	# возврат страницы
-	msg = ""
-	if request.method == "POST":
-		pwd = request.form.get("pwd")
-		salt = request.form.get("salt")
-		num_char = request.form.get("num")
+    # возврат страницы
+    msg = ""
+    if request.method == "POST":
+        pwd = request.form.get("pwd")
+        salt = request.form.get("salt")
+        num_char = request.form.get("num")
 
-		msg = hashing(pwd + salt, num_char)
+        msg = hashing(pwd + salt, num_char)
 
-		loging(request, msg)
+        loging(request, msg)
 
-	return render_template("product.html", message=msg)
+    return render_template("product.html", message=msg)
+
 
 @app.route("/contact")
 def contact_page():
-	with sql.connect("logs.db") as db:
-		cursor = db.cursor()
-		cursor.execute(
-			"SELECT * FROM log_table"
-		)
-		data = cursor.fetchall()
-	return render_template("contact.html", data=data)
+    with sql.connect("logs.db") as db:
+        cursor = db.cursor()
+        cursor.execute(
+            "SELECT * FROM log_table"
+        )
+        data = cursor.fetchall()
+    return render_template("contact.html", data=data)
+
 
 # точка входа
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
---
+++ static/test0.py
@@ -0,0 +1,164 @@
+# a = [1, 2, 4]
+# a.insert(2, 3)
+# print(a)
+
+# str = "Wow! This is string!"
+# a = max(str)
+# print(a)
+
+
+# a = ["a", "b", "c"]
+# x = "11".join(a)
+# print(x)
+
+# mylist = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# x = mylist[-6:-1]
+# print(x)
+
+# print("AyuShi".lower())
+# print("AyuShi".upper())
+
+# mydict = {"a": 1, "b": 2, "c": 3, "e": 5}
+# print(mydict.keys())
+
+
+# print("Hello"[:-1])
+
+# import os
+# print(os.getlogin())
+
+
+# a = [1, 2, 3, 4]
+# x = a.reverse()
+# print(x)
+
+# a = 5
+# while a > 0:
+#     print(a, end=" ")
+#     a -= 1
+
+
+# a = 5
+# while a >= 5 and a <= 55:
+#     print(a, end=" ")
+#     a += 2
+
+# n = int(input())
+# for i in range(n):
+#     for j in range(n):
+#         print('*', end="")
+#     print()
+
+
+# print(*[1, 2, 3, 4, 5], sep="<>", end="\t")
+# print('\t', end ='')
+
+# for row in range(10):
+#     for column in range(10):
+#         print(column, end=" ")
+#     print()
+
+
+# for row in range(10):
+#     for column in range(10):
+#         print(row, end=" ")
+#     print()
+
+
+# for row in range(10):
+#     for column in range(row+1):
+#         print(column, end=" ")
+#     print()
+
+# for row in range(10):
+#     for j in range(row):
+#         print(" ", end=" ")
+#     for column in range(10-row):
+#         print(column, end=" ")
+#     print()
+
+
+# for row in range(1, 10):
+#     for column in range(1, 10):
+#         if row*column < 10:
+#             print(" ", end="")
+#         print(column*row, end=" ")
+#     print()
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# a < 10 and b < 10 and c < 10 and d < 10 and a <= b and c <= d
+# for i in range(a, b+1):
+#     for j in range(c, d+1):
+#         if i*j < 10:
+#             print(" ", end="")
+#         print(i*j, end=" ")
+#     print()
+
+# for i in range(7, 11):
+#     for j in range(5, 7):
+#         print(i*j, end=" ")
+#     print()
+
+
+# for i in range(1, 10):
+#     for j in range(1, 10):
+#         if i*j < 10:
+#             print(" ", end="")
+#         print(i*j, end=" ")
+#     print()
+
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# for j in range(c, d+1):
+#     print(end="\t")
+#     print(j, end="")
+# print()
+# for i in range(a, b+1):
+#     print(i, end="\t")
+#     for j in range(c, d+1):
+#         print(i*j, end="\t")
+#     print()
+
+
+# a = int(input())
+# b = int(input())
+# s = 0
+# c = 0
+# for i in range(a, b + 1):
+#     if i % 3 == 0:
+#         s += i
+#         c += 1
+# print(s/c)
+
+
+# x = input().lower()
+# a = x.count('c')
+# b = x.count('g')
+# c = len(x)
+# print((a + b) / c * 100)
+
+# A = [1, 2, 3, 4, 5]
+# for x in A:
+#     print(x)
+#     x += 1
+#     print(x)
+# print(A)
+
+
+# A = [1, 2, 3, 4, 5]
+# for k in range(5):
+#     A[k] = A[k] * A[k]
+# print(A)
+
+# s = input()
+# for x in range(s):
+#     s[n] =
+
+
